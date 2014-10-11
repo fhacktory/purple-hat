ab0d607839602c8bfa51b019531826c67ee65f0b
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-10-11 14:30:32 +0200

    Master server fixed

--- app/src/main/java/purplehat/fr/purplehat/MasterServer.java
+++ app/src/main/java/purplehat/fr/purplehat/MasterServer.java
@@ -7,18 +7,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.InetSocketAddress;
+import java.util.Collection;
 
 /**
  * Created by jmcomets on 11/10/14.
  */
-public class PeerServer extends WebSocketServer {
-    public PeerServer(InetSocketAddress address) {
+public class MasterServer extends WebSocketServer {
+    public MasterServer(InetSocketAddress address) {
         super(address);
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         // TODO gérer les connexions
+        conn.send("ok");
     }
 
     @Override
@@ -44,5 +46,22 @@ public class PeerServer extends WebSocketServer {
     @Override
     public void onError(WebSocket conn, Exception ex) {
         // TODO gérer les erreurs
+    }
+
+    /**
+     * Sends <var>text</var> to all currently connected WebSocket clients.
+     *
+     * @param text
+     *            The String to send across the network.
+     * @throws InterruptedException
+     *             When socket related I/O errors occur.
+     */
+    public void broadcast(String text) {
+        Collection<WebSocket> con = connections();
+        synchronized (con) {
+            for (WebSocket c : con) {
+                c.send(text);
+            }
+        }
     }
 }
